3159d0e252bb55ac302bc26eabe75e2c3839c605
DHEERAJ <86879344+dheeraj7-13@users.noreply.github.com>
2024-11-16 12:15:52 +0530

    Update stack.java

--- stack/stack.java
+++ stack/stack.java
@@ -27,19 +27,60 @@
 
 class AStack 
 {
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Stack s = new Stack(4);
         char ch;
 
-        do 
-        {
+        do {
             System.out.println("\nStack array implementation");
-            System.out.println("1.Push to stack");
-            System.out.println("2.Pop out of stack");
-            System.out.println("3.Peek operation");
-            System.out.println("4.Display operation");
-        }
-    }
-}
+            System.out.println("1. Push to stack");
+            System.out.println("2. Pop out of stack");
+            System.out.println("3. Peek operation");
+            System.out.println("4. Display operation");
+
+            int choice = scan.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter the element to push:");
+                    System.out.println("option 1");
+
+                    // int element = scan.nextInt();
+                    // s.push(element);
+                    break;
+
+                case 2:
+                    // int poppedElement = s.pop();
+                    System.out.println("option 2");
+
+                    // if (poppedElement != -1) {
+                        // System.out.println("Popped element: " + poppedElement);
+                    // }
+                    break;
+
+                case 3:
+                    // int peekedElement = s.peek();
+                    System.out.println("option 3");
+
+                    // if (peekedElement != -1) {
+                    //     System.out.println("Peeked element: " + peekedElement);
+                    // }
+                    break;
+
+                case 4:
+                    // s.display();
+                    System.out.println("option 4");
+
+                    break;
+
+                default:
+                    System.out.println("Incorrect Choice");
+            }
+
+            System.out.print("Do you want to continue? (Y/N): ");
+            ch = scan.next().charAt(0);
+        } while (ch != 'n' || ch != 'N');
+
+        scan.close();
+    }}
