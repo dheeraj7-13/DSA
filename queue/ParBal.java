7fd1e73815d2d9c2a0d78d024dbce9a18fe6ff7a
DHEERAJ <86879344+dheeraj7-13@users.noreply.github.com>
2024-12-02 16:53:58 +0530

    Add files via upload

---
+++ queue/ParBal.java
@@ -0,0 +1,49 @@
+/* 
+	Name: Vaibhav Katkar
+	Roll No: 1524
+	Unit 3: Stacks and Queue
+	Program: Parenthesis Balancing - Stack Application
+*/
+
+public class ParBal {
+
+
+    public static boolean isBalanced(String expr) {
+        // Create a stack 
+        char[] stack = new char[expr.length()];
+        int tos = -1; // top of stack index
+
+
+        for (int i = 0; i < expr.length(); i++) {
+            char ch = expr.charAt(i);
+
+ 
+            if (ch == '(') {
+                tos++;
+                stack[tos] = ch;
+            }
+         
+            else if (ch == ')') {
+              
+                if (tos == -1) {
+                    return false;
+                }
+                tos--; 
+            }
+        }
+
+
+        return tos == -1;
+    }
+
+
+    public static void main(String[] args) {
+        String expr = "(a+b)+(c+d)";
+
+        if (isBalanced(expr)) {
+            System.out.println("Parentheses are balanced");
+        } else {
+            System.out.println("Parentheses are not balanced");
+        }
+    }
+}
